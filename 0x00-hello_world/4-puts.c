833285eb123dde4e8524a820b93d75df533a2548
Daniel Omenka Odeh <danielodehomenka@gmail.com>
2023-03-16 15:54:29 -0700

    print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #!/bin/bash
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+
 int main() {
     puts("Programming is like building a multilingual puzzle");
     return 0;
