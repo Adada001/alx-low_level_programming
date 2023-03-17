016e034880cdf149cc492d65061500c60e1079e7
Daniel Omenka Odeh <danielodehomenka@gmail.com>
2023-03-17 16:28:33 -0700

    print base ten

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints base 10 numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	for (i = 0; i < 19; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
