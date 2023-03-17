f22a8af09c5132bb927620cdf81a107d28e7f5b3
Daniel Omenka Odeh <danielodehomenka@gmail.com>
2023-03-17 15:40:55 -0700

    print azAZ

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char azAZ[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(azAZ[i]);
+	}
+	for (i = 0; i > 26; i++)
+	{
+		putchar(azAZ[i]);
+	}
+	putchar('\n');
+	return (0);
+}
