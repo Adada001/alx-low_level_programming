cc571984b78915a378b57c4af074af9bf306cfd2
Daniel Omenka Odeh <danielodehomenka@gmail.com>
2023-03-17 15:54:33 -0700

    ommit q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet except q and e.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char az[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+	
+	while (ax <= 'z')
+	{
+		if (az == 'e' || az == 'q')
+			++ch;
+		putchar(az);
+		++az;
+	}
+	putchar('\n');
+	return (0);
+}
