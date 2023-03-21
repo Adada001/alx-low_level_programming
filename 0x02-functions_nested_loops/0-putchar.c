1b4db8c808329d28628c2f5e84c14d0d8d1ab1a9
Daniel Omenka Odeh <danielodehomenka@gmail.com>
2023-03-21 14:10:38 -0700

    print putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "_putchar.h"
 
 /**
  * main - Prints the _putchar.
@@ -12,8 +12,8 @@ int main(void)
 
 	for (i = 0; i < 8; i++)
 	{
-		putchar(pc[i]);
+		_putchar(pc[i]);
 	}
-	putchar('\n');
+	_putchar('\n');
 	return (0);
 }
