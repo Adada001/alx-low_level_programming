9e6f2df3233538198278e6a2b4d7e925cee7b8e9
Daniel Omenka Odeh <danielodehomenka@gmail.com>
2023-03-21 14:38:22 -0700

    print putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,11 +5,13 @@
  * Return: Always 0 (success)
  */
 
-int main(void) {
-    char str[] = "_putchar\n";
-    int i;
-    for (i = 0; str[i] != '\0'; i++) {
-        putchar(str[i]);
-    }
+int main(void) 
+{
+	char str[] = "_putchar\n";
+	int i;
+	for (i = 0; str[i] != '\0'; i++) 
+	{
+		putchar(str[i]);
+	}
     return 0;
 }
