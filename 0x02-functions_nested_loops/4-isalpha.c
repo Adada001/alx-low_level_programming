6387138408e16988ab3a8a42756d939d5797393a
Daniel Omenka Odeh <danielodehomenka@gmail.com>
2023-03-22 15:54:45 -0700

    check for alphabet

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,8 +7,8 @@
 
 int _isalpha(int ch)
 {
-	if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch<= 'Z'))
+	if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
 		return (1);
 	else
 		return (0);
-}		
+}
