655b654fd378bac4ff22822c70f1abb3a2c6e4d7
Daniel Omenka Odeh <danielodehomenka@gmail.com>
2023-03-22 16:09:33 -0700

    print signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign - print + if n greater than zero
+ * print 0 is n is zero
+ * print '-' if n is less than zero
+ *
+ * Return - 1 if greater than zero, 0 if n is zero, "-1" if less than zero
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return(1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return('0');
+	}
+	else
+	{
+		_putchar('-');
+		return("-1");
+	}
+}
