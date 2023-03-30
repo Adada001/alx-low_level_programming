dc10a3ff1a66f550ffe65708c7f4ee8be9cfd91e
Daniel Omenka Odeh <danielodehomenka@gmail.com>
2023-03-30 12:52:50 -0700

    concat two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,11 +9,15 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0, dest_len = 0;
+	int i, dl;
 
-	while (dest[index++])
-		dest_len++;
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
+	i = 0
+
+	while (dest[i])
+		i++;
+	for (dl = 0; dl < n && src[dl] != '\0'; dl++)
+		dest[i + dl] = src[dl];
+	dest[i + dl] = '\0';
+
 	return (dest);
 }
