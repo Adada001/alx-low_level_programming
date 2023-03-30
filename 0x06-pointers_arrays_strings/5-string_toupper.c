c011314da402bb0f97ca63651895bcf21b0c973b
Daniel Omenka Odeh <danielodehomenka@gmail.com>
2023-03-29 17:05:00 -0700

    conver to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - Changes all lowercase letters
+ *                  of a string to uppercase.
+ * @str: The string to be changed.
+ *
+ * Return: A pointer to the changed string.
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 'a' + 'A';
+		}
+	}
+
+	return s;
+}
