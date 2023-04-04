2df749bb76036e0035c256ffb06a1b78d7d194b0
Daniel Omenka Odeh <danielodehomenka@gmail.com>
2023-04-04 12:26:33 -0700

    find matching byte

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,15 +11,19 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	int i = 0, j = 0;
+	int i, j;
 
-	for (s[i] != '\0'; i++)
+	i = 0
+	while (s[i] != '\0')
 	{
-		for (accept[j] != '\0'; j++)
+		j = 0;
+		while (accept[j] != '\0')
 		{
 			if (s[i] == accept[j])
 				return (s + i);
+			j++;
 		}
+		i++;
 	}
 
 	return ('\0');
