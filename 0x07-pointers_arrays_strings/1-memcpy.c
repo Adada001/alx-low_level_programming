a263ab300f941b5e61559f286382326448cc7519
Daniel Omenka Odeh <danielodehomenka@gmail.com>
2023-04-04 08:07:38 -0700

    copy bytes

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+/**
+ * -memcpy - copy n bytes fro mem src to mem dest
+ *  @dest: destination mem area
+ *  @src: source memory
+ *  @n: number of bytes to copy
+ *
+ *  Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
