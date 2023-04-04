e4fb8b408e3abd16fc174f607b68d0d640cfa7ff
Daniel Omenka Odeh <danielodehomenka@gmail.com>
2023-04-04 09:06:58 -0700

    len of string

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,30 +1,30 @@
-#include "main.h"
 /**
- * _strspn - gets length of substring
- * @s: string to be searched
- * @accept: string containing char to searched
+ * _strspn - gets the length of a prefix substring
+ * @s: pointer to the string to be searched
+ * @accept: pointer to the string containing the characters to search for
  *
- * Return: number of bytes
+ * Return: number of bytes in the initial segment of s which consist only of bytes from accept
  */
-unsigned in _strspn(char *s, char *accept)
+unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j, len;
+    unsigned int i, j, len;
 
-	len = 0;
+    len = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		for (j = 0; accept[j] != '\0'; j++)
-		{
-			if (s[i] == accept[j])
-			{
-				len++;
-				break;
-			}
-		}
-		if (accept[j] == '\0')
-			return(len);
-	}
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        for (j = 0; accept[j] != '\0'; j++)
+        {
+            if (s[i] == accept[j])
+            {
+                len++;
+                break;
+            }
+        }
 
-	return (len);
+        if (accept[j] == '\0')
+            return (len);
+    }
+
+    return (len);
 }
