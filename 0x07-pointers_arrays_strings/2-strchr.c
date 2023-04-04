8da0810b41c4d026556f3f11fe0b0ba2e5787362
Daniel Omenka Odeh <danielodehomenka@gmail.com>
2023-04-04 08:45:04 -0700

    search for char in string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - search char in a string
+ * @s: string
+ * @c: char to search
+ *
+ * Return: point to the first occurance of c found or Null if not found.
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] = '0\'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+
+	return ('\0');
+}
