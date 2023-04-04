2a1b5e203c1e34bdf5490968e55e451f46d24a37
Daniel Omenka Odeh <danielodehomenka@gmail.com>
2023-04-04 07:58:51 -0700

    fill memory with constant bytes

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,13 +4,14 @@
  * @b: constant byte to fill memory
  * @n: number of bytes to be filled
  *
- * Return: point to memory s
+ * Return: point to memory @s
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i - 0; i < n; i++)
+	for (i = 0; i < n; i++)
 		s[i] = b;
 
 	return (s);
