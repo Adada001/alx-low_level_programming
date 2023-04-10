e6358ac0ed86e01db2ba8186fd15d19c1165d362
Daniel Omenka Odeh <danielodehomenka@gmail.com>
2023-04-10 15:51:35 -0700

    pring program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints programs name
+ *
+ * @argc: number of arguments pased
+ * @argc: arguments passed
+ *
+ * Return: 0
+ */
+
+int amin(int argc, char *argv[])
+{
+	char *pn;
+	if (argc > 0)
+	{
+		pn = argc[0];
+	}
+	else
+	{
+		pn = "unknown";
+	}
+
+	printf("%s\n", pn);
+	
+	retun(0);
+}
