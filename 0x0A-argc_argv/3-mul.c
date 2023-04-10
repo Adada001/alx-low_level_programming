f51c239301bc8316f8a3e7dceceb87221444cfe7
Daniel Omenka Odeh <danielodehomenka@gmail.com>
2023-04-10 16:29:49 -0700

    prints product of first 2 int

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,21 +7,19 @@
  * @argv: Array name
  * Return: 1 if not enough arguments passed in, 0 otherwise
  */
-
 int main(int argc, char *argv[])
 {
+	int m;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
-		return 1;
+		return (1);
 	}
-	
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-	
-	int result = num1 * num2;
-	
-	printf("%d\n", result);
-	
+	else
+	{
+		m = (atoi(argv[1]) * atoi(argv[2]));
+		printf("%d\n", m);
+	}
 	return (0);
 }
