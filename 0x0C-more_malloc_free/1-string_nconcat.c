d6bf1cf537e59d41c80c4f7ebedd9448ebcfa946
Daniel Omenka Odeh <danielodehomenka@gmail.com>
2023-04-13 10:21:02 -0700

    concat two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,37 +8,34 @@
  *
  * Return: A ponter to the concatenated string or NULL if mem allocation fails
  */
-char *string_nconcat(char *s1, char *2, unsigned int n)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len1 = 0, len2 = 0;
 	char *concat;
+	unsigned int len = n, index;
 
-	/* Treat NULL as an empty string */
-	s1 = s1 ? s1 : "";
-	s2 = s2 ? s2 : "";
+	if (s1 == NULL)
+		s1 = "";
 
-	/*Get the ken of s1 and s2 */
-	while (s1[len1])
-		len1++;
-	while (s2[len2])
-		len2++;
+	if (s2 == NULL)
+		s2 = "";
 
-	/* Allocate memory for the concatd string */
-	if (n >= len2)
-		concat = malloc(sizeof(char) * (len1 + len2 +1));
-	else
-		concat = malloc(sizeof(char) * (len1 + n + 1));
-	if  (concat == NULL)
+	for (index = 0; s1[index]; index++)
+		len++;
+
+	concat = malloc(sizeof(char) * (len + 1));
+
+	if (concat == NULL)
 		return (NULL);
 
-	/* cpy sq and teh first n bytes of s2 into the new string */
-	for (unsigen int i = 0; i < len1; i++)
-		concat[i] =s1[i];
-	for (unsigned int i = 0; i < n && s2[i]; i++)
-		concat[len1 +i] = s2[i];
+	len = 0;
 
-	/* Add a null terminator to the end of the new string */
-	concat[len1 + (>= len2 ? len2 : n)] = /\0;
+	for (index = 0; s1[index]; index++)
+		concat[len++] = s1[index];
+
+	for (index = 0; s2[index] && index < n; index++)
+		concat[len++] = s2[index];
+
+	concat[len] = '\0';
 
 	return (concat);
 }
