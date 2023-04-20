5a01ec43419e84eb5b26179499e3a19fe0710914
Daniel Omenka Odeh <danielodehomenka@gmail.com>
2023-04-20 12:19:11 -0700

    each element of an array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-#include <stddef.h>
+
 /**
  * array_iterator - executes a function on each element of an array
  *
@@ -11,11 +11,12 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (array && action)
+	if (array == NULL || action == NULL)
+		return;
+	
+	while (size-- > 0)
 	{
-		for (size_t i = 0; i < size; i++)
-		{
-			action(array[i]);
-		}
+		action(*array);
+		array++;
 	}
 }
