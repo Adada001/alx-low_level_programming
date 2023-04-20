d84369dd811ecc011edbba6bc2025d296e93d90f
Daniel Omenka Odeh <danielodehomenka@gmail.com>
2023-04-20 12:20:40 -0700

    each element of an array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,8 +6,6 @@
  * @array: pointer to an array of integers
  * @size: size of the array
  * @action: pointer to a function that takes an int argument and returns void
- *
- * Return: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
