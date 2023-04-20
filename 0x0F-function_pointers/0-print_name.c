706746bb0aa09dd349ab1634a840451760811df0
Daniel Omenka Odeh <danielodehomenka@gmail.com>
2023-04-20 12:00:18 -0700

    functions

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include "function_pointers.h"
 /**
  * print_name - prints a name using a provided function
  *
@@ -8,6 +9,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-    if (name && f)
-        f(name);
+	if (name && f)
+		f(name);
 }
