2f14bf2b0a663c9e570ceb33e9e8cf2904422c98
Daniel Omenka Odeh <danielodehomenka@gmail.com>
2023-04-20 11:53:08 -0700

    functions

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+/**
+ * print_name - prints a name using a provided function
+ *
+ * @name: pointer to name string
+ * @f: pointer to function that takes a char pointer as argument and returns void
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+    if (name && f)
+        f(name);
+}
