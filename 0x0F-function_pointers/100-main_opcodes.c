71fb240755bd73ca2773b2dcf338242104437b53
Daniel Omenka Odeh <danielodehomenka@gmail.com>
2023-04-20 13:07:49 -0700

    print opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -14,20 +14,20 @@ int main(int argc, char *argv[])
 {
 	int bytes, i;
 	char *main_ptr;
-	
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	
+
 	bytes = atoi(argv[1]);
 	if (bytes < 0)
 	{
 		printf("Error\n");
 		return (2);
 	}
-	
+
 	main_ptr = (char *)main;
 	for (i = 0; i < bytes; i++)
 	{
@@ -37,6 +37,6 @@ int main(int argc, char *argv[])
 		else
 			printf(" ");
 	}
-	
+
 	return (0);
 }
