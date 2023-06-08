735b7079303c22a2a789358a137d1108e13c02c3
Daniel Odeh <danielomenkaodeh@gmail.com>
2023-06-08 15:57:40 -0700

    cal len of string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strken_recursion - calculates the len of a string
+ * _strlen_recursion - calculates the len of a string
  * @s: the string to calculate the len
  *
  * Return: the string len
@@ -10,5 +10,5 @@ int _strlen_recursion(char *s)
 	if(*s == '\0')
 		return 0;
 
-	return 1 + _strken_recursion(s + 1);
+	return 1 + _strlen_recursion(s + 1);
 }
