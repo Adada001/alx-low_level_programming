2bb6558ce3e3a5005864912047d50acb06319e7f
Daniel Odeh <danielomenkaodeh@gmail.com>
2023-06-08 16:07:14 -0700

    cal factorial of a number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * factorial - calculates the factorial of a given number
+ * @n: the number to calculate.
+ *
+ * Return: the factorial numer or -1 if its an error
+ */
+int facotial(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	if (n == 0)
+		return (1);
+
+	return n * factorial(n - 1);
+}
