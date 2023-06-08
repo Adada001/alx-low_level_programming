3c66f71093ca9ae37a1d0d4980ace34efd65bfc9
Daniel Odeh <danielomenkaodeh@gmail.com>
2023-06-08 16:15:06 -0700

    cal pow of x to y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _pow_recursion - cal the value of x raised to power of y
+ * @x: base value
+ * @y: exponent value
+ *
+ * Return: the result of x raised to y or -1 if its an error
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+
+	if (y == 0)
+		return (1);
+
+	return x * _pow_recursion(x, y - 1);
+}
